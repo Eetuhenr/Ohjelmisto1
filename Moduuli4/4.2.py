1931861e5743dc6f9aa89cb3141bb1e32e2f8c14
Eetuhenr <eetuhenr@metropolia.fi>
2025-03-05 16:42:51 +0200

    Add files via upload

---
+++ Moduuli4/4.2.py
@@ -0,0 +1,7 @@
+tuumat = float(input("syötä tuumamäärä"))
+while tuumat <= 0:
+   senttimetrit = tuumat * 2,54
+   print(f"{tuumat} tuumaa on {senttimetrit} senttimetriä")
+else:
+    print("ohjelma lopetettu")
+
